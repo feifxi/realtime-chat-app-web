f812c2390686ce949490974acb6d200a68658e3e
hello <feoy1234@gmail.com>
2025-04-12 23:06:40 +0700

    update root api

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -5,7 +5,7 @@ import { io } from 'socket.io-client'
 import { useChatStore } from "./useChatStore";
 import notificationSound from '../assets/sounds/notification.mp3'
 
-const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5001' : '/';
+const BASE_URL = import.meta.env.VITE_API_ROOT;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
